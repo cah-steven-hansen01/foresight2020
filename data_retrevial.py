bcf8c73c26a44be28ebff1caf68927893caf4f5a
cah-steven-hansen01 <85251300+cah-steven-hansen01@users.noreply.github.com>
2021-11-30 11:22:27 -0600

    Added files via upload

---
+++ data_retrevial.py
@@ -0,0 +1,21 @@
+import pyodbc
+import pandas as pd
+import os
+import itertools
+import re
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class Data_Retrevial:
+    def pull_plantstar(self):
+        pass
+    def pull_archived_so(self):
+        pass
+    def pull_ncs(self):
+        pass
+    def pull_cff_schedule(self):
+        pass
+    def pull_sff_schedule(self):
+        pass
+    
